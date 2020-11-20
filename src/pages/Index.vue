c361874613bc5342ce53747f2f4d4febf01402cf
kanglin <kanglin@yonyou.com>
2020-11-20 21:46:38 +0800

    feat:功能完成

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,112 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+    <Layout>
+        <!-- Page Header -->
+        <header class="masthead"
+                :style="{backgroundImage: `url(${GRIDSOME_API_URL}${general.cover.url})`}">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <div class="site-heading">
+                            <h1>{{ general.title }}</h1>
+                            <span class="subheading">{{ general.sub }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Main Content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="post-preview"
+                         v-for="edge in $page.posts.edges"
+                         :key="edge.node.id">
+                        <g-link :to="'/post/' + edge.node.id">
+                            <h2 class="post-title">
+                                {{ edge.node.title }}
+                            </h2>
+                            <h3 class="post-subtitle">
+                                Problems look mighty small from 150 miles up
+                            </h3>
+                        </g-link>
+                        <p class="post-meta">Posted by
+                            <a href="#">{{ edge.node.created.firstname + edge.node.created.lastname }}</a>
+                            on {{ edge.node.created_at }}
+                        </p>
+                        <p>
+                            <span v-for="tag in edge.node.tags"
+                                  :key="tag.id">
+                                <g-link :to="'/tag/' + tag.id">{{tag.title}}</g-link>
+                                &nbsp;&nbsp;
+                            </span>
+                        </p>
+                        <hr>
+                    </div>
+                    <!-- Pager -->
+                    <!-- <div class="clearfix">
+                        <a class="btn btn-primary float-right"
+                           href="#">Older Posts &rarr;</a>
+                    </div> -->
+                    <pager :info="$page.posts.pageInfo"></pager>
+                </div>
+            </div>
+        </div>
+    </Layout>
 </template>
+<page-query>
+query ($page: Int) {
+    posts: allStrapiPost (perPage: 2, page: $page) @paginate {
+        pageInfo {
+            totalPages
+            currentPage
+        }
+        edges {
+            node {
+                id
+                title
+                created_at
+                created {
+                    id
+                    firstname
+                    lastname
+                }
+                tags {
+                    id
+                    title
+                }
+            }
+        }
+    }
 
+    general: allStrapiGeneral {
+        edges {
+            node {
+                id
+                title
+                sub
+                cover {
+                    url
+                }
+            }
+        }
+    }
+}
+</page-query>
 <script>
+import { Pager } from 'gridsome'
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
+    name: 'HomePage',
+    metaInfo: {
+        title: 'Hello, world!'
+    },
+    components: {
+        Pager
+    },
+    computed: {
+        general () {
+            return this.$page.general.edges[0].node
+        }
+    }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
